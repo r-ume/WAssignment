abce295594e3e255df7b0890db9e8786ded7f227
r-ume <baymax.blackandwhite@gmail.com>
2017-03-05 14:38:50 +0900

    put a fake array to make sure names in the array get displayed in tableview

--- WAssignment/ViewController.swift
+++ WAssignment/ViewController.swift
@@ -13,9 +13,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    var allRepository = [
+        "swift","laravel","codeigniter","rails","objective-c",
+    ]
+    
+    var searchResultRepository = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchResultRepository = allRepository
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,11 +43,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // MARK: - UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return searchResultRepository.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = searchResultRepository[indexPath.row]
         return cell
     }
 
